a058d18fa8a45c383fe8bd808dd1608a2faa96a0
sravani <sravanikaranam23@gmail.com>
2025-03-01 23:19:16 +0530

    commit

---
+++ Day-12-Multi-SG-ForLoop/example-1-multi-inbound-same-cidr/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+    region = "ap-south-1"
+  
+}
+resource "aws_instance" "web" {
+  ami                    = "ami-0d682f26195e9ec0f"      #change ami id for different region
+  instance_type          = "t2.micro"
+  key_name               = "mykeypair"              #change key name as per your setup
+ 
+  tags = {
+    Name = "project-MainEc2"
+  }
+
+  root_block_device {         #we can give custom value here 
+    volume_size = 40
+  }
+}
+
+resource "aws_security_group" "devops-project-sravani" {
+  name        = "devops-project-sravani"
+  description = "Allow TLS inbound traffic"
+
+  ingress = [
+    for port in [22, 80, 443, 8080, 9000, 3000, 8082, 8081] : {
+      description      = "inbound rules"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "devops-project-sravani"
+  }
+}
